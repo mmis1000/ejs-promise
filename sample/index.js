4a5d8d8e9b39faece4ddd2fc921dcf14e74e09a3
mmis1000 <mmis1000@yahoo.com.tw>
2016-06-11 16:08:37 +0000

    fix sample

--- sample/index.js
+++ sample/index.js
@@ -2,10 +2,10 @@ var express = require('express'),
     ejs = require('../lib/ejs.js');
 var app = express();
 app.engine('.html', ejs.__express);
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs-stream');
 app.get('/', function(req, res) {
 	res.render(__dirname + '/index.html', {header: 'Hello EJS'});
 });
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening at http://localhost:3000');
 });
